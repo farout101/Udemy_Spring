adf14cc883f636cdf9723d52a3a2f7b32661d2f7
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-04-08 13:10:30 +0630

    second commit

--- hibernate/src/main/java/net/farout/Main.java
+++ hibernate/src/main/java/net/farout/Main.java
@@ -1,8 +1,30 @@
 package net.farout;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Student s1 = new Student();
+        s1.setsName("new");
+        s1.setRollNo(1);
+        s1.setsAge(19);
+
+        Configuration cfg = new Configuration();
+        cfg.addAnnotatedClass(net.farout.Student.class);
+        cfg.configure();
+
+        SessionFactory sf = cfg.buildSessionFactory();
+        Session session = sf.openSession();
+
+        Transaction transaction = session.beginTransaction();
+
+        session.save(s1);
+
+        transaction.commit();
+
+        System.out.println(session.get(net.farout.Student.class, s1.getRollNo()));
     }
-    
 }
---
+++ hibernate/src/main/java/net/farout/Student.java
@@ -0,0 +1,49 @@
+package net.farout;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int rollNo;
+    private String sName;
+    private int sAge;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "rollNo=" + rollNo +
+                ", sName='" + sName + '\'' +
+                ", sAge=" + sAge +
+                '}';
+    }
+
+    public int getRollNo() {
+        return rollNo;
+    }
+
+    public void setRollNo(int rollNo) {
+        this.rollNo = rollNo;
+    }
+
+    public String getsName() {
+        return sName;
+    }
+
+    public void setsName(String sName) {
+        this.sName = sName;
+    }
+
+    public int getsAge() {
+        return sAge;
+    }
+
+    public void setsAge(int sAge) {
+        this.sAge = sAge;
+    }
+}
