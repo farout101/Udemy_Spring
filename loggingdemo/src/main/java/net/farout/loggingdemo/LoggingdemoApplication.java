9dfb972c67415034c9c675632a09279cf30590c5
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-04-09 15:32:02 +0630

    logging tested

--- loggingdemo/src/main/java/net/farout/loggingdemo/LoggingdemoApplication.java
+++ loggingdemo/src/main/java/net/farout/loggingdemo/LoggingdemoApplication.java
@@ -3,10 +3,16 @@ package net.farout.loggingdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.logging.LogManager;
+
 @SpringBootApplication
 public class LoggingdemoApplication {
 
     public static void main(String[] args) {
+        LogManager.getLogManager().reset();
+        java.util.logging.Logger rootLogger = java.util.logging.LogManager.getLogManager().getLogger("");
+        rootLogger.setLevel(java.util.logging.Level.OFF);
+
         SpringApplication.run(LoggingdemoApplication.class, args);
     }
 
