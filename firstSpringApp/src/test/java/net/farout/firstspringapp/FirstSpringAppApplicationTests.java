1caee0a392a698d0105d5f8ecd9c61eaa97bbf06
Hacker Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-04-09 10:17:52 +0630

    first spring app created

---
+++ firstSpringApp/src/test/java/net/farout/firstspringapp/FirstSpringAppApplicationTests.java
@@ -0,0 +1,13 @@
+package net.farout.firstspringapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSpringAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
