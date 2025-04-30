f70b723bc723809d66fd9c931ce1b65f4fb064c0
farout101 <phyozawlinn1852020@gmail.com>
2025-04-30 14:14:26 +0630

    microservice

---
+++ Microservice-QuizApp/question-service/src/main/java/net/farout/questionservice/QuestionServiceApplication.java
@@ -0,0 +1,13 @@
+package net.farout.questionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestionServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuestionServiceApplication.class, args);
+    }
+
+}
---
+++ Microservice-QuizApp/question-service/src/test/java/net/farout/questionservice/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package net.farout.questionservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
