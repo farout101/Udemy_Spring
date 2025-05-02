fdecf82e942705ceda404943944745ddf1793767
farout101 <phyozawlinn1852020@gmail.com>
2025-05-02 09:22:58 +0630

    new service added

---
+++ Microservice-QuizApp/question-service/src/main/java/net/farout/questionservice/service/QuestionService.java
@@ -0,0 +1,82 @@
+package net.farout.questionservice.service;
+
+import net.farout.questionservice.dao.QuestionDao;
+import net.farout.questionservice.model.Question;
+import net.farout.questionservice.model.QuestionWrapper;
+import net.farout.questionservice.model.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class QuestionService {
+    @Autowired
+    QuestionDao questionDao;
+
+    public ResponseEntity<List<Question>> getAllQuestions() {
+        try {
+            return new ResponseEntity<>(questionDao.findAll(), HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+    }
+
+    public ResponseEntity<List<Question>> getQuestionsByCategory(String category) {
+        try {
+            return new ResponseEntity<>(questionDao.findByCategory(category),HttpStatus.OK);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return new ResponseEntity<>(new ArrayList<>(), HttpStatus.BAD_REQUEST);
+
+    }
+
+    public ResponseEntity<String> addQuestion(Question question) {
+        questionDao.save(question);
+        return new ResponseEntity<>("success",HttpStatus.CREATED);
+    }
+
+    public ResponseEntity<List<Integer>> getAllQuestionsForQuiz(String categoryName, int numQuestions) {
+       List<Integer> questions = questionDao.findRandomQuestionsByCategory(categoryName, numQuestions);
+       return new ResponseEntity<>(questions,HttpStatus.OK);
+    }
+
+    public ResponseEntity<List<QuestionWrapper>> getQuestionsFromId(List<Integer> questionIds) {
+        List<QuestionWrapper> questionWrappers = new ArrayList<>();
+
+        List<Question> questions = new ArrayList<>();
+
+        for(Integer id : questionIds) {
+            questions.add(questionDao.findById(id).get());
+        }
+
+        for(Question question : questions) {
+           QuestionWrapper questionWrapper = new QuestionWrapper();
+           questionWrapper.setId(question.getId());
+           questionWrapper.setQuestionTitle(question.getQuestionTitle());
+           questionWrapper.setOption1(question.getOption1());
+           questionWrapper.setOption2(question.getOption2());
+           questionWrapper.setOption3(question.getOption3());
+           questionWrapper.setOption4(question.getOption4());
+           questionWrappers.add(questionWrapper);
+        }
+
+        return new ResponseEntity<>(questionWrappers, HttpStatus.OK);
+    }
+
+    public ResponseEntity<Integer> getScore(List<Response> responses) {
+        int right = 0;
+        for(Response response : responses) {
+            Question question = questionDao.findById(response.getId()).get();
+            if(response.getResponse().equals(question.getRightAnswer()))
+                right++;
+        }
+
+        return new ResponseEntity<>(right, HttpStatus.OK);
+    }
+}
---
+++ Microservice-QuizApp/quiz-service/src/main/java/net/farout/quizservice/QuizServiceApplication.java
@@ -0,0 +1,13 @@
+package net.farout.quizservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuizServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuizServiceApplication.class, args);
+    }
+
+}
