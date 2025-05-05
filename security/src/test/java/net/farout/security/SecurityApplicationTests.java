4c612df04f421082ee9dea9584e6eb7c02fd9362
farout101 <phyozawlinn1852020@gmail.com>
2025-05-05 13:52:03 +0630

    security

---
+++ security/src/test/java/net/farout/security/SecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package net.farout.security;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
