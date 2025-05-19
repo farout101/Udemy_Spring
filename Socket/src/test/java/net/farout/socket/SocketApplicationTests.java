513f63f9afc37eba5d035e6c3e39b51a63550509
Mg Pyuue <phyozawlinn1852020@gmail.com>
2025-05-19 12:35:23 +0630

    socket added

---
+++ Socket/src/test/java/net/farout/socket/SocketApplicationTests.java
@@ -0,0 +1,13 @@
+package net.farout.socket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
